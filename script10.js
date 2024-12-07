b993ed4c6672b38da65447973a075fc5865d4e9b
Nixk01 <nikunjjadhav786@gmail.com>
2024-12-08 04:02:35 +0530

    Scripts added

---
+++ script10.js
@@ -0,0 +1,19 @@
+// for loop
+
+// for(let i = 0;i < 5;i++){  
+//     console.log("I'm in loop", i);
+//     }
+
+// console.log("loop finished");
+
+const nm = ['Nixk', 'Jack', 'Dhaval'];
+
+for(let i = 0; i < nm.length;i++){
+    // console.log(nm[i]);
+    let html = `<div> ${nm[i]} </div>`;
+    console.log(html);
+}
+
+console.log("loop finished");
+
+
---
+++ script11.js
@@ -0,0 +1,27 @@
+// while loop
+
+/*
+const nm1 = ['Nixk', 'Jack', 'Dhaval'];
+
+let i =0;
+
+// while(i<5){
+//     console.log('In loop', i);
+//     i++;
+// }
+
+while(i<nm1.length){
+    console.log('In loop', nm1[i]);
+    i++;
+}
+
+*/
+
+// Do while loop
+
+let i=3;
+
+do{
+    console.log('In loop', i);
+    i++;
+}while(i<5);
